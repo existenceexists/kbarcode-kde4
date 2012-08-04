453999ed722ed37ba4e326f122b5267e579b1517
František Brožka <kbarcode@centrum.cz>
2012-08-04 10:36:39 +0200

    Fix text items
    Comment out "srt.setTextWidth( w );" so that text items behave the same as in the old Kbarcode.

--- src/textitem.cpp
+++ src/textitem.cpp
@@ -110,7 +110,7 @@ void TextItem::draw(QPainter* painter)
         painter->setPen( Qt::black );
         /*srt.setWidth( painter, w );
         srt.draw( painter, 0, 0, QRect( 0, 0, w, h ), cg );*/// -!F: keep
-        srt.setTextWidth( w );
+        //srt.setTextWidth( w );
         srt.drawContents( painter, QRect( 0, 0, w, h ) );
     }
     else
@@ -186,7 +186,7 @@ void TextItem::drawPreview(QPainter* painter)
         painter->setPen( Qt::black );
         /*srt.setWidth( painter, w );
         srt.draw( painter, rect().x(), rect().y(), QRect( rect().x(), rect().y(), w, h ), cg );*/// -!F: keep
-        srt.setTextWidth( w );
+        //srt.setTextWidth( w );
         srt.drawContents( painter, QRect( 0, 0, w, h ));
     }
     else
